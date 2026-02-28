2ed047311f1fc96a333f232f870e41145f23499f
Haseeb Alvi <haseebalvi91@gmail.com>
2026-02-28 13:12:48 +0500

    Made it a general product

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -121,7 +121,7 @@ export default function USLPage() {
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 px-4">
       <div className="w-full max-w-md">
         <h1 className="text-2xl font-sans text-primary">
-          Sign in to Mealio
+          Authenticate
         </h1>
 
         {method === "phone" ? (
